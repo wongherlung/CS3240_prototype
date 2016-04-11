d11027550278a13b2078fba71de3f5841f1e585c
Wiwid <nurwidyauj@Nurwidyas-MacBook-Pro.local>
2016-04-11 19:28:59 +0800

    list of reviews added. layout fixed.

--- js/review.js
+++ js/review.js
@@ -1,22 +1,35 @@
 
 $(document).ready(function() {
+    // list of modules for search reviews
+    var moduleCodes = Object.keys(modules);
+    var values = moduleCodes.map(function(code) {
+        return code + ' - ' + modules[code].name;
+    });
 
-  var moduleCodes = Object.keys(modules);
-  var values = moduleCodes.map(function(code) {
-    return code + ' - ' + modules[code].name;
-  });
+    $('#module-review-select').html('');
+    $('#module-review-select').append('<option value=""></option>');
+    values.forEach(function(value) {
+        $('#module-review-select').append('<option value="'+value+'">'+value+'</option>');
+    });
+    $('#module-review-select').chosen({'width': '100%', 'white-space': 'nowrap'});
 
-  $('#module-review-select').html('');
-  $('#module-review-select').append('<option value=""></option>');
-  values.forEach(function(value) {
-    $('#module-review-select').append('<option value="'+value+'">'+value+'</option>');
-  });
+    // list of past modules for create new review
+    var pastModuleCodes = Object.keys(pastModules);
+    var pastValues = pastModuleCodes.map(function(code){
+        return code;
+    });
+   
+    $('#select-module-for-review').html('');
+    $('#select-module-for-review').append('<option value=""></option>');
+    pastValues.forEach(function(pastValue) {
+        $('#select-module-for-review').append('<option value="'+pastValue+'">'+pastValue+'</option>');
+    });
+    $('#select-module-for-review').chosen({'width': '100%', 'white-space': 'nowrap'});
 
-  $('#module-review-select').chosen({'width': '100%', 'white-space': 'nowrap'});
+});
 
-  $('#show-modal-btn').click(function() {
+$('#show-modal-btn').click(function() {
     $('#create-review-modal').modal('show');
-  });
 });
 
 $('#submit-review-btn').click(function(){
@@ -29,72 +42,87 @@ $('#submit-review-btn').click(function(){
     $('#main-review-box').css('border-top-color', '#F6BB42');
 })
   
-  $('#search-review-btn').click(function(){
+$('#search-review-btn').click(function(){
     $('#sem-taken-filter').prop('disabled', false);
     $('#diff-filter').prop('disabled', false);
     $('#recommendation-filter').prop('disabled', false);
     $('#apply-filter-btn').prop('disabled', false);
+    
     var input = $('#module-review-select').val();
+    input = input.substr(0, input.indexOf('-'));
+    input = input.trim();
     generateReview(input);
 })
 
 function generateReview(code) {
-    $('.warning-text-container').css('padding-left', '50px');
-    $('.warning-text-container').css('padding-right', '50px');
-    $('#warning-text').remove();
-    $('#show-modal-btn').remove();
-    if($('#review-header').length){
-        $('#review-header').remove();
-    }
-    $('#main-review-box').append('<h3 id="review-header">Review(s) for '+code+'</h3>');
-    var reviewTemplate = '';
-    reviewTemplate += '<table class="table">';
+    $('#warning-text').html('');
+    //$('#show-modal-btn').remove();
     
-    var tempcode = reviews[code];
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">Matric No. :</td>';
-    reviewTemplate += '<td align="left">'+tempcode.matric+'</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">Sem taken :</td>';
-    reviewTemplate += '<td align="left">'+tempcode.sem+'</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">Difficulty :</td>';
-    reviewTemplate += '<td align="left">'+tempcode.difficulty+'</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">Rating :</td>';
-    reviewTemplate += '<td align="left">'+tempcode.recommendation+'</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">Comment :</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '</table>';
-    reviewTemplate += '<table>';
-    reviewTemplate += '<tr>';
-    reviewTemplate += '<td align="left">'+tempcode.comment+'</td>';
-    reviewTemplate += '</tr>';
-    reviewTemplate += '</table>';
-    $('#main-review-box').append(reviewTemplate);
+    $('#review-header').html('Review(s) for '+code);
+    $('.list-of-reviews').html('');
+    
+    var reviewTemplate = '';
+    var found = false;
+    for(var cd in reviews){
+        if(code==cd){
+            found = true;
+            var tempcode = reviews[code];
+            
+            for(var i=0; i<tempcode.length; i++){
+                var diff = tempcode[i].difficulty;
+                var recom = tempcode[i].recommendation;
+                reviewTemplate += '<div class="row review-item">';
+                reviewTemplate += '<div class="col-md-3">';
+                reviewTemplate += '<p id="matric">'+tempcode[i].matric+'</p>';
+                reviewTemplate += '<p>AY '+tempcode[i].ay +' semester '+tempcode[i].sem+'</p>';
+                reviewTemplate += '<div class="col-md-6" id="diff-box-'+i+'">';
+                reviewTemplate += '<p>Difficulty:</p>';
+                reviewTemplate += '<p>'+tempcode[i].difficulty+'</p>';
+                reviewTemplate += '</div>';
+                reviewTemplate += '<div class="col-md-6" id="rec-box-'+i+'">';
+                reviewTemplate += '<p>Likes:</p>';
+                reviewTemplate += '<p>'+tempcode[i].recommendation+'</p>';
+                reviewTemplate += '</div>';
+                reviewTemplate += '</div>';
+                reviewTemplate += '<div class="col-md-9">';
+                reviewTemplate += '<p>'+tempcode[i].comment+'</p>';
+                reviewTemplate += '</div></div>';
+            }
+        }
+    }
+    if(!found){
+        $('#warning-text').html('No review has been made for this module.');
+    }
+    $('.list-of-reviews').append(reviewTemplate);
 }
 
 var reviews = {
-    "CS1231": {
-        "matric": "A0098103J",
+    "CS1231": [
+        {"matric": "A0098103J",
+        "ay": "2012/2013",
         "sem": "2",
-        "difficulty": "3 - Moderate",
-        "recommendation": "4 - Recommended",
+        "difficulty": "Moderate",
+        "recommendation": "Recommended",
         "comment": "This module is very fun and not that difficult compared to other CS modules. The lecturer is very fun although sometimes he could give us challenging problems. All in all, this module is recommended to take!"
-    },
+        },
+        {"matric": "A0102867U",
+        "ay": "2014/2015",
+        "sem": "2",
+        "difficulty": "Moderate",
+        "recommendation": "Neutral",
+        "comment": "Simply math, no coding, for those who love math might be happy to take this mod."
+        },
+    ],    
     "IS4221": {
         "matric": "A0098103J",
+        "ay": "2014/2015",
         "sem": "1",
-        "difficulty": "4 - Somewhat Difficult",
-        "recommendation": "2 - Not Recommended",
+        "difficulty": "Somewhat Difficult",
+        "recommendation": "Not Recommended",
         "comment": "Lecturer is very lousy. Don't take this unless you really don't have other mods to take!!!!"
     }
 }
+
 
 var modules = {
   "CS1101S": {
@@ -312,5 +340,158 @@ var modules = {
     "type": ["UE", "Breadth"],
     "credits": 4,
     "workload": "5-4-3-2-4"
+  }
+};
+
+var pastModules = {
+  "CS1101S": {
+    "year": 1,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS1231": {
+    "year": 1,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "MA1521": {
+    "year": 1,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "MA1101R": {
+    "year": 1,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "TR2201": {
+    "year": 1,
+    "semester": 1,
+    "type": "UE",
+    "grade": "A"
+  },
+  "LSM1302": {
+    "year": 1,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "ST2334": {
+    "year": 1,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "IS1103": {
+    "year": 1,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2020": {
+    "year": 1,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2100": {
+    "year": 2,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2101": {
+    "year": 2,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2103T": {
+    "year": 2,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS3216": {
+    "year": 2,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2103": {
+    "year": 2,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2102": {
+    "year": 2,
+    "semester": 1,
+    "type": "Breadth",
+    "grade": "A"
+  },
+  "CS2105": {
+    "year": 2,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2106": {
+    "year": 2,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS2107": {
+    "year": 2,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS3230": {
+    "year": 2,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "LSM1303": {
+    "year": 2,
+    "semester": 2,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS3103": {
+    "year": 3,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "LSM1301": {
+    "year": 3,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "GEK1520": {
+    "year": 3,
+    "semester": 1,
+    "type": "GEM",
+    "grade": "A"
+  },
+  "CS4238": {
+    "year": 3,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
+  },
+  "CS3235": {
+    "year": 3,
+    "semester": 1,
+    "type": "Core",
+    "grade": "A"
   }
 };
