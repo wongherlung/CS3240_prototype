f91fb9d59aa167fe9109ad9e701297490867c3a5
Wiwid <nurwidyauj@Nurwidyas-MacBook-Pro.local>
2016-04-17 17:53:19 +0800

    filter and input validation added.

--- js/review.js
+++ js/review.js
@@ -25,23 +25,60 @@ $(document).ready(function() {
         $('#select-module-for-review').append('<option value="'+pastValue+'">'+pastValue+'</option>');
     });
     $('#select-module-for-review').chosen({'width': '100%', 'white-space': 'nowrap'});
-
+    
 });
 
 $('#show-modal-btn').click(function() {
+    $('#select-module-for-review').val(''); //still error
+    $('#lecturer').val('');
+    $("#rate-difficulty").val('');
+    $("#rate-recommendation").val('');
+    $("#comment").val('');
+    $(".select-mod-review").removeClass("has-error");
+    $('.input-lecturer').removeClass("has-error");
+    $(".rate-diff").removeClass("has-error");
+    $(".rate-rec").removeClass("has-error");
+     $(".input-comment").removeClass("has-error");
+    $(".error-text").html('');
     $('#create-review-modal').modal('show');
+    
 });
 
 $('#submit-review-btn').click(function(){
-    $('#create-review-modal').modal('hide');
-    var successText = 'Your review has been submitted! <br>You can now search for reviews by selecting the module in search bar above. Or you can click below button to create more reviews.</br>'
-    $('#warning-text').html(successText);
-    $('#module-review-select').prop('disabled', false);
-    $('#module-review-select').trigger("chosen:updated");
-    $('#search-review-btn').prop('disabled', false);
-    $('#main-review-box').css('border-top-color', '#F6BB42');
-})
-  
+    var mod = $("#select-module-for-review").val();
+    var lecturer = $('#lecturer').val();
+    var diff = $("#rate-difficulty").val();
+    var rec = $("#rate-recommendation").val();
+    var comment = $("#comment").val();
+    
+    if(mod=='' || lecturer=='' || diff=='' || rec=='' || comment==''){
+        $(".error-text").append("<p>Please fill in the required fields!</p>");
+        if(mod == ''){
+            $(".select-mod-review").addClass("has-error"); //doesnt appear
+        }
+        if(lecturer==''){
+            $('.input-lecturer').addClass("has-error");
+        }
+        if(diff==''){
+            $(".rate-diff").addClass("has-error");
+        }
+        if(rec==''){
+            $(".rate-rec").addClass("has-error");
+        }
+        if(comment==''){
+            $(".input-comment").addClass("has-error");
+        }
+    } else {
+        $('#create-review-modal').modal('hide');
+        var successText = 'Your review has been submitted! <br>You can now search for reviews by selecting the module in search bar above. Or you can click below button to create more reviews.</br>'
+        $('#warning-text').html(successText);
+        $('#module-review-select').prop('disabled', false);
+        $('#module-review-select').trigger("chosen:updated");
+        $('#search-review-btn').prop('disabled', false);
+        $('#main-review-box').css('border-top-color', '#F6BB42');
+    }
+});
+
 $('#search-review-btn').click(function(){
     $('#sem-taken-filter').prop('disabled', false);
     $('#diff-filter').prop('disabled', false);
@@ -52,12 +89,62 @@ $('#search-review-btn').click(function(){
     input = input.substr(0, input.indexOf('-'));
     input = input.trim();
     generateReview(input);
-})
+});
+
+$('#apply-filter-btn').click(function(){
+    var semester = $('#sem-taken-filter').val();
+    var diff = $('#diff-filter').val();
+    var rating = $('#recommendation-filter').val();
+    
+    var listLength = $('#review-list li').length;
+    var semFilter = false;
+    var diffFilter = false;
+    var recFilter = false;
+    
+    for(var i=0; i<listLength; i++){
+        $('#review-item-'+i).removeClass('hide');
+    }
+    
+    if(semester!=''){
+        semFilter = true;
+    }
+    if(diff!=''){
+        diffFilter = true;
+    }
+    if(rating!=''){
+        recFilter = true;
+    }
+    
+    if(semFilter || diffFilter || recFilter){
+        for(var i=0; i<listLength; i++){
+            if(semFilter){
+                var thisSem = $('#sem-'+i).text();
+                thisSem = thisSem.substring(22,23);
+                if(thisSem!=semester){
+                    $('#review-item-'+i).addClass('hide');
+                }
+            }
+            if(diffFilter){
+                var thisDiff = $('#diff-'+i).text(); 
+                if(thisDiff!=diff){
+                    $('#review-item-'+i).addClass('hide');
+                }
+            }
+            if(recFilter){
+                var thisRec = $('#recom-'+i).text();
+                if(thisRec!=rating){
+                    $('#review-item-'+i).addClass('hide');
+                }
+            }
+        }
+    }
+    
+});
 
 function generateReview(code) {
+    
     $('#warning-text').html('');
     //$('#show-modal-btn').remove();
-    
     $('#review-header').html('Review(s) for '+code);
     $('.list-of-reviews').html('');
     
@@ -67,31 +154,36 @@ function generateReview(code) {
         if(code==cd){
             found = true;
             var tempcode = reviews[code];
+            reviewTemplate += '<ul id="review-list">';
             
             for(var i=0; i<tempcode.length; i++){
                 var diff = tempcode[i].difficulty;
                 var recom = tempcode[i].recommendation;
-                reviewTemplate += '<div class="row review-item">';
+                reviewTemplate += '<li>';
+                reviewTemplate += '<div class="row review-item" id="review-item-'+i+'">';
                 reviewTemplate += '<div class="col-md-3">';
                 reviewTemplate += '<p id="matric">'+tempcode[i].matric+'</p>';
-                reviewTemplate += '<p>AY '+tempcode[i].ay +' semester '+tempcode[i].sem+'</p>';
-                reviewTemplate += '<div class="col-md-6" id="diff-box-'+i+'">';
+                reviewTemplate += '<p id="sem-'+i+'">AY '+tempcode[i].ay +' semester '+tempcode[i].sem+'</p>';
+                reviewTemplate += '<p>Lecturer : '+tempcode[i].lecturer+'</p>';
+                reviewTemplate += '<div class="col-md-6">';
                 reviewTemplate += '<p>Difficulty:</p>';
-                reviewTemplate += '<p>'+tempcode[i].difficulty+'</p>';
+                reviewTemplate += '<p id="diff-'+i+'">'+tempcode[i].difficulty+'</p>';
                 reviewTemplate += '</div>';
-                reviewTemplate += '<div class="col-md-6" id="rec-box-'+i+'">';
-                reviewTemplate += '<p>Likes:</p>';
-                reviewTemplate += '<p>'+tempcode[i].recommendation+'</p>';
+                reviewTemplate += '<div class="col-md-6">';
+                reviewTemplate += '<p>Rating:</p>';
+                reviewTemplate += '<p id="recom-'+i+'">'+tempcode[i].recommendation+'</p>';
                 reviewTemplate += '</div>';
                 reviewTemplate += '</div>';
                 reviewTemplate += '<div class="col-md-9">';
                 reviewTemplate += '<p>'+tempcode[i].comment+'</p>';
-                reviewTemplate += '</div></div>';
+                reviewTemplate += '</div></div></li>';
             }
+            
+            reviewTemplate += '</ul>';
         }
     }
     if(!found){
-        $('#warning-text').html('No review has been made for this module.');
+       $('#warning-text').html('No review has been made for this module.');
     }
     $('.list-of-reviews').append(reviewTemplate);
 }
@@ -100,6 +192,7 @@ var reviews = {
     "CS1231": [
         {"matric": "A0098103J",
         "ay": "2012/2013",
+        "lecturer": "Chan Mun Choon",
         "sem": "2",
         "difficulty": "Moderate",
         "recommendation": "Recommended",
@@ -107,22 +200,58 @@ var reviews = {
         },
         {"matric": "A0102867U",
         "ay": "2014/2015",
+        "lecturer": "Chan Mun Choon",
         "sem": "2",
         "difficulty": "Moderate",
         "recommendation": "Neutral",
         "comment": "Simply math, no coding, for those who love math might be happy to take this mod."
         },
     ],    
-    "IS4221": {
-        "matric": "A0098103J",
+    "IS4231": [
+        {"matric": "A0098103J",
         "ay": "2014/2015",
+        "lecturer": "Prof X",
         "sem": "1",
         "difficulty": "Somewhat Difficult",
         "recommendation": "Not Recommended",
         "comment": "Lecturer is very lousy. Don't take this unless you really don't have other mods to take!!!!"
-    }
+        },
+        {"matric": "A0102873L",
+        "ay": "2013/2014",
+        "lecturer": "Prof Y",
+        "sem": "1",
+        "difficulty": "Moderate",
+        "recommendation": "Neutral",
+        "comment": "Not a good module. Most of the topics are textbook-material and not much "
+        },
+    ],
+    "MA1521": [
+        {"matric": "A0102367U",
+        "ay": "2011/2012",
+        "lecturer": "Wang Fei",
+        "sem": "1",
+        "difficulty": "Somewhat Difficult",
+        "recommendation": "Neutral",
+        "comment": "Even though the title is Calculus for Computing, there is nothing to do with computing. Don't be fooled! This is just simply calculus. My lecturer was Wang Fei and he loves to give tricky and challenging questions. But, overall his grading is quite lenient."
+        },
+        {"matric": "A0176342S",
+        "ay": "2012/2013",
+        "lecturer": "Wang Fei",
+        "sem": "2",
+        "difficulty": "Somewhat Easy",
+        "recommendation": "Recommended",
+        "comment": "I love math. This module is awesome!"
+        },
+        {"matric": "A0092348L",
+        "ay": "2013/2014",
+        "lecturer": "Wang Fei",
+        "sem": "1",
+        "difficulty": "Moderate",
+        "recommendation": "Neutral",
+        "comment": "All math and calculus. Not related to computing and coding st all. Prof Wang Fei is very talented in teaching even though he likes to give tricky problems."
+        }
+    ]
 }
-
 
 var modules = {
   "CS1101S": {
